258fc830390b44c20f7be3cd7b6e5fe69b13b192
christophermowrey <christophermowrey@gmail.com>
2022-07-22 07:14:31 -0400

    Finished

--- script.js
+++ script.js
@@ -1,61 +1,77 @@
 // Assignment Code
 var generateBtn = document.querySelector('#generate');
 var lowerChar = "abcdefghijklmnopqrstuvwxyz";
-var upperChar = lowerChar.toUpperCase();
+var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numChar = "1234567890";
-var specChar = "[ ~`!@#$%^&*()_+-=,./<>?;':\"{}\|]"
+var specChar = "[ ~`!@#$%^&*()_+-=,./<>?;':\"{}\|]";
+var i = 0;
+var j = 0;
+var k = 0;
+var l = 0;
 // /" escapes the quote, allowing inclusion";;
-console.log(specChar)
+console.log(specChar);
 
 function generatePassword() {
-  var password = '';
+  console.log("function runs");
 
   // TODO: add code to generate the password here
+  var password = ""
   var lowerChoice = window.prompt(
     "Would you like to use lower case letters? (y/n)"
-  )
+  );
   var upperChoice = window.prompt(
     "Would you like to use upper case letters? (y/n)"
-  )
+  );
   var numChoice = window.prompt(
     "Would you like to use numbers? (y/n)"
-  )
-  var specChar = window.prompt(
+  );
+  var specChoice = window.prompt(
     "Would you like to use special characters? (y/n)"
-  )
+  );
   var charLength = window.prompt(
     "Using the number pad, choose a password length between 8-128"
-  )
-  while (lowerChoice === "y") {
+  );
+  while (lowerChoice === "y" && i < 1) {
     password = password.concat(lowerChar);
     console.log(password);
-  }
-  while (upperChoice === "y") {
+    i++;
+  };
+  while (upperChoice === "y" && j < 1) {
     password = password.concat(upperChar);
     console.log(password);
-  }
-  while (numChoice === "y") {
+    j++;
+  };
+  while (numChoice === "y" && k < 1) {
     password = password.concat(numChar);
     console.log(password);
-  }
-  while (specChar === "y") {
+    k++;
+  };
+  while (specChoice === "y" && l < 1) {
     password = password.concat(specChar);
     console.log(password);
-  }
-  // for (var i = 0; i <= passwordLength; i++) {
-  //   var randomNumber = Math.floor(Math.random() * password.length);
-  //   password += password.substring(randomNumber, randomNumber +1);
-  //  }
-
+    l++;
+  };
+  var chars = password;
+  var password = "";
+  console.log(password);
+    // below substring generator from: https://dev.to/code_mystery/random-password-generator-using-javascript-6a
+  for (var m = 0; m <= charLength; m++) {
+    console.log(m);
+    console.log(chars);
+        var randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber +1);
+    console.log(password);
+   };
+   console.log(password);
   return password;
-};
+ };
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector('#password');
 
-  passwordText.value = password;
+  passwordText.value = password; 
 };
 
 // Add event listener to generate button
